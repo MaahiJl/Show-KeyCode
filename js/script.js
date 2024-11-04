c02951f5f80b9f97aeeee6699cf55552f9ff1d48
Negin Jalali <neginjalali28@gmail.com>
2024-11-04 17:45:14 +0330

    final

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿let $ = document
+let title = $.querySelector('title')
+
+let starter = $.getElementById('starter');
+let ascii = $.getElementById('ascii');
+let infos = $.getElementById('infos');
+let key = $.getElementById('key');
+let Location = $.getElementById('location');
+let which = $.getElementById('which');
+let code = $.getElementById('code');
+let keyCode = $.getElementById('keyCode');
+
+document.body.addEventListener('keydown', function (event) {
+	event.preventDefault();
+
+    key.innerHTML = event.key
+    Location.innerHTML = event.location
+    which.innerHTML = event.which
+    code.innerHTML = event.code
+    keyCode.innerHTML = event.key
+
+	starter.style.display = 'none'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+    console.log(event);
+})
